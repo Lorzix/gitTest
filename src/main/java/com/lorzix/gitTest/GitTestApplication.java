ecbb5d2652feb7d20f397846fb2823e393af3810
Lorzix <Lorzix@.com>
2022-01-24 17:57:36 +0800

    hot-fix first commit

--- src/main/java/com/lorzix/gitTest/GitTestApplication.java
+++ src/main/java/com/lorzix/gitTest/GitTestApplication.java
@@ -5,5 +5,6 @@ public class GitTestApplication {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
+        System.out.println("hello world3");
     }
 }
