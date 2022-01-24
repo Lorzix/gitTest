dbca2660c85c946d3b460a9a5aa3013426a722b9
Lorzix <Lorzix@.com>
2022-01-24 18:01:43 +0800

    master fix commit

--- src/main/java/com/lorzix/gitTest/GitTestApplication.java
+++ src/main/java/com/lorzix/gitTest/GitTestApplication.java
@@ -6,5 +6,7 @@ public class GitTestApplication {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hello world4 master fix");
+
     }
 }
